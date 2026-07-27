9d946929f26c5b24f63b35cee96aff7711b113f5
gilded <knukles30@gmail.com>
2026-07-26 20:20:20 -0600

    chore: bump version to 1.2.2

--- tests/sanity.sh
+++ tests/sanity.sh
@@ -96,7 +96,7 @@ run_windows_compat_smoke() {
         ANI_CLI_STATE_NAME=ani-cli-mx LOCALAPPDATA="$local_app_data_env" \
         ANI_CLI_PLAYER=debug ./ani-cli-mx-core -V)"
 
-    [ "$version_output" = "1.2.1" ]
+    [ "$version_output" = "1.2.2" ]
     [ -f "$local_app_data/ani-cli-mx/ani-hsts" ]
     grep -q 'GIT_INSTALL_ROOT' ani-cli-mx.cmd
     grep -q 'ANI_CLI_PACKAGE_MANAGER=scoop' ani-cli-mx.cmd
